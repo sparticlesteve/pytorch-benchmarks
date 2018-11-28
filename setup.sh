73fc7599c940cac3799be5133b03f9df3266c607
Steve Farrell <sparticlesteve@gmail.com>
2018-11-28 11:34:35 -0800

    moved to my conda env v1.0-intel

--- setup.sh
+++ setup.sh
@@ -2,5 +2,8 @@
 export OMP_NUM_THREADS=32
 export KMP_AFFINITY="granularity=fine,compact,1,0"
 export KMP_BLOCKTIME=1
-export BENCHMARK_VERSION=v0.4.1
-module load pytorch-mpi/v0.4.1
+export BENCHMARK_VERSION=v1.0-intel
+
+installDir=$SCRATCH/conda/pytorch/v1.0.0-intel
+conda activate $installDir
+export LD_LIBRARY_PATH=$installDir/lib:$LD_LIBRARY_PATH
