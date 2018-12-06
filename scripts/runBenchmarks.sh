d04f508ac67040f1a621067fe7d01bc3b4b422f5
Steve Farrell <sparticlesteve@gmail.com>
2018-12-06 14:21:55 -0800

    add srun to python commands

--- scripts/runBenchmarks.sh
+++ scripts/runBenchmarks.sh
@@ -8,7 +8,7 @@
 . setup.sh
 module list
 
-python ./train.py configs/benchmark_alexnet.yaml
-python ./train.py configs/benchmark_vgg11.yaml
-python ./train.py configs/benchmark_resnet50.yaml
-python ./train.py configs/benchmark_inceptionV3.yaml
+srun python ./train.py configs/benchmark_alexnet.yaml
+srun python ./train.py configs/benchmark_vgg11.yaml
+srun python ./train.py configs/benchmark_resnet50.yaml
+srun python ./train.py configs/benchmark_inceptionV3.yaml
