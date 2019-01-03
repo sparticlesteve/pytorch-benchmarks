af81f14becaecbb757f8133cd4cdec6e1a613d8c
Steve Farrell <sparticlesteve@gmail.com>
2019-01-02 21:35:12 -0800

    updating to cori module

--- setup.sh
+++ setup.sh
@@ -4,6 +4,4 @@ export KMP_AFFINITY="granularity=fine,compact,1,0"
 export KMP_BLOCKTIME=1
 export BENCHMARK_VERSION=v1.0-intel
 
-installDir=$SCRATCH/conda/pytorch/v1.0.0-intel
-conda activate $installDir
-export LD_LIBRARY_PATH=$installDir/lib:$LD_LIBRARY_PATH
+module load pytorch/v1.0.0-intel
