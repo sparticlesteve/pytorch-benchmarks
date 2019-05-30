e76d037c8d4bf06d150979264c37795a4b61bdbb
Steve Farrell <sparticlesteve@gmail.com>
2019-05-30 09:48:44 -0700

    using pytorch/v1.1.0

--- setup.sh
+++ setup.sh
@@ -2,6 +2,6 @@
 export OMP_NUM_THREADS=32
 export KMP_AFFINITY="granularity=fine,compact,1,0"
 export KMP_BLOCKTIME=1
-export BENCHMARK_RESULTS_PATH=$SCRATCH/pytorch-benchmarks/v1.0.0-intel
+export BENCHMARK_RESULTS_PATH=$SCRATCH/pytorch-benchmarks/v1.1.0-intel
 
-module load pytorch/v1.0.0-intel
+module load pytorch/v1.1.0
