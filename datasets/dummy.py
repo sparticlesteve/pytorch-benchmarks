5ca220606241ddc17b3d3548a690d256c9b61cec
Steve Farrell <sparticlesteve@gmail.com>
2019-05-31 17:41:32 -0700

    seeding in dummy dataset for reproducibility and consistency across ranks

--- datasets/dummy.py
+++ datasets/dummy.py
@@ -13,6 +13,8 @@ def get_datasets(n_train=1024, n_valid=1024,
                  input_shape=[3, 32, 32], target_shape=[],
                  n_classes=None):
     """Construct and return random number datasets"""
+    initial_seed = torch.initial_seed()
+    torch.manual_seed(0)
     train_x = torch.randn([n_train] + input_shape)
     valid_x = torch.randn([n_valid] + input_shape)
     if n_classes is not None:
@@ -21,6 +23,7 @@ def get_datasets(n_train=1024, n_valid=1024,
     else:
         train_y = torch.randn([n_train] + target_shape)
         valid_y = torch.randn([n_valid] + target_shape)
+    torch.manual_seed(initial_seed)
     train_dataset = TensorDataset(train_x, train_y)
     valid_dataset = TensorDataset(valid_x, valid_y)
     return train_dataset, valid_dataset
