2fb16ca62640ae79f0f045320b395e8cd6d9e274
Steve Farrell <sparticlesteve@gmail.com>
2020-02-25 12:04:18 -0800

    fix parse.py to work with new paths

--- parse.py
+++ parse.py
@@ -39,8 +39,8 @@ def load_results(results_dirs):
         print(results_dir)
 
         # Extract hardware, software, from path
-        m = re.match('(.*)-(.*)-n(\d+)', os.path.basename(results_dir))
-        hw, sw, ranks = m.group(1), m.group(2), int(m.group(3))
+        m = re.match('(.*)-(.*)-(.*)-n(\d+)', os.path.basename(results_dir))
+        hw, sw, backend, ranks = m.group(1), m.group(2), m.group(3), int(m.group(4))
 
         # Use all subdirectories as models
         models = [m for m in os.listdir(results_dir)
@@ -48,7 +48,7 @@ def load_results(results_dirs):
         for model in models:
             model_subdir = os.path.join(results_dir, model)
             results.append(load_result(model_subdir, hardware=hw, version=sw,
-                                       model=model, ranks=ranks))
+                                       backend=backend, model=model, ranks=ranks))
 
     return pd.DataFrame(results)
 
