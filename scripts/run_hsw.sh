2b5a56ba3de1b2a04fafd4de45f618dc7daa6d7d
Steve Farrell <sparticlesteve@gmail.com>
2020-05-21 10:15:25 -0700

    add command line settings and printouts to the three run scripts

--- scripts/run_hsw.sh
+++ scripts/run_hsw.sh
@@ -11,20 +11,57 @@ set -e
 
 # Options
 version=v1.5.0
-clean=true
 backend=mpi
 models="alexnet resnet50 lstm cnn3d"
-if [ $# -ge 1 ]; then models=$@; fi
+clean=true
+usage="$0 --version VERSION --backend BACKEND --models \"MODELS ...\" --clean CLEAN"
+
+# Parse command line options
+while (( "$#" )); do
+    case "$1" in
+        --version)
+            version=$2
+            shift 2
+            ;;
+        --backend)
+            backend=$2
+            shift 2
+            ;;
+        --models)
+            models=$2
+            shift 2
+            ;;
+        --clean)
+            clean=$2
+            shift 2
+            ;;
+        *)
+            echo "Usage: $usage"
+            exit 1
+            ;;
+    esac
+done
 
 # Configuration
 export OMP_NUM_THREADS=32
 export KMP_AFFINITY="granularity=fine,compact,1,0"
 export KMP_BLOCKTIME=1
+# TODO: improve this to avoid using env var in the config file
 export BENCHMARK_RESULTS_PATH=$SCRATCH/pytorch-benchmarks/results/hsw-$version-$backend-n${SLURM_JOB_NUM_NODES}
 if $clean; then
     [ -d $BENCHMARK_RESULTS_PATH ] && rm -rf $BENCHMARK_RESULTS_PATH
 fi
+
+# Print settings
+echo "Running PyTorch benchmarks with"
+echo "version $version"
+echo "backend $backend"
+echo "models $models"
+echo "clean $clean"
+echo "writing outputs to $BENCHMARK_RESULTS_PATH"
+
 module load pytorch/$version
+module list
 
 # Run each model
 for m in $models; do
