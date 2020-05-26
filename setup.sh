a6263de864039f7cffa18212849101d39dd95c42
Steve Farrell <sparticlesteve@gmail.com>
2020-05-26 16:35:11 -0700

    remove unused setup file

--- setup.sh
+++
@@ -1,7 +0,0 @@
-# Example environment setup script for Cori
-export OMP_NUM_THREADS=32
-export KMP_AFFINITY="granularity=fine,compact,1,0"
-export KMP_BLOCKTIME=1
-export BENCHMARK_RESULTS_PATH=$SCRATCH/pytorch-benchmarks/v1.1.0-intel
-
-module load pytorch/v1.1.0
